7d723309b63c5872b9550b6698f029958054cb22
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-04-27 11:59:10 -0300

    vue-meta fix, meta tags added

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
   data(){
     return {
       apiUrl: "https://api.ennes.dev",
-      maintenance: true
+      maintenance: false
     }
   },
   components: {
@@ -45,40 +45,40 @@ export default {
   },
   metaInfo: {
     // Children can override the title.
-    title: 'My Page Title',
+    title: 'portfolio',
     // Result: My Page Title ← My Site
     // If a child changes the title to "My Other Page Title",
     // it will become: My Other Page Title ← My Site
-    titleTemplate: '%s ← My Site',
+    titleTemplate: 'ennes@dev:$ %s',
     link: {rel: 'canonical', href: 'https://www.ennes.dev'},
     // Define meta tags here.
     meta: [
       {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
+      {description: `
+        Web developer Gustavo Ennes website
+      `},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'description', content: 'Gustavo Ennes portfolio'},
-      {property: 'og:title', content: 'Gustavo Ennes portfolio'},
+      {name: 'description', content: 'Gustavo Ennes website'},
+      {property: 'og:title', content: 'Web developer Gustavo Ennes website'},
       {property: 'og:site_name', content: 'Ennes Dev'},
       // The list of types is available here: http://ogp.me/#types
       {property: 'og:type', content: 'website'},
       // Should the the same as your canonical link, see below.
-      {property: 'og:url', content: 'https://www.ennes.dev'},
+      {property: 'og:url', content: 'Web developer Gustavo Ennes website'},
       //{property: 'og:image', content: 'https://www.my-site.com/my-special-image.jpg'},
       // Often the same as your meta description, but not always.
       {property: 'og:description', content: 'Gustavo Ennes portfolio'},
 
-      // Twitter card
-      {name: 'twitter:card', content: 'portfolio'},
-      {name: 'twitter:site', content: 'https://www.ennes.dev'},
-      {name: 'twitter:title', content: 'Gustavo Ennes portfolio'},
-      {name: 'twitter:description', content: 'Gustavo Ennes portfolio'},
-      // Your twitter handle, if you have one.
-      {name: 'twitter:creator', content: '@alligatorio'},
-      // {name: 'twitter:image:src', content: 'https://www.my-site.com/my-special-image.jpg'},
-
+      
       // Google / Schema.org markup:
-      {itemprop: 'name', content: 'Gustavo Ennes portfolio'},
-      {itemprop: 'description', content: 'Gustavo Ennes portfolio'},
+      {itemprop: 'name', content: 'ennes.dev'},
+      {itemprop: 'description', content: 'Web developer Gustavo Ennes website'},
       // {itemprop: 'image', content: 'https://www.my-site.com/my-special-image.jpg'}
+      {itemprop: 'jobTitle', content: 'full-stack web developer'},
+      {itemprop: 'knowsAbout', content: 'web development'},
+      {itemprop: 'aditionalName', content: 'Web developer Gustavo Ennes website'},
+      {itemprop: 'address', content: 'ilha solteira, são paulo - brasil'},
+      {itemprop: 'email', content: 'gustavo@ennes.dev'},
     ]
   }
 }
