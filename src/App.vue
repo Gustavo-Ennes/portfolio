5d51e0071df03ce1322d1bba570d1c566407919d
Gustavo Ennes <gustavo_ennes@yahoo.com>
2021-05-02 08:06:16 -0300

    updated vue-meta tags, added webpack plugin to sitemap

--- src/App.vue
+++ src/App.vue
@@ -50,20 +50,28 @@ export default {
   },
   metaInfo: {
     // Children can override the title.
-    title: 'portfolio',
+    title: `
+        Full-stack web developer freelancer, Javascript developer, Python developer, Node developer, 
+        Vue developer, Django developer, Gustavo Ennes portfolio, Gustavo Ennes dev, Gustavo Ennes projects, 
+        Gustavo Ennes freelancer
+      `,
     // Result: My Page Title ← My Site
     // If a child changes the title to "My Other Page Title",
     // it will become: My Other Page Title ← My Site
-    titleTemplate: 'ennes@dev:$ %s',
+    titleTemplate: 'Gustavo Ennes %s',
     link: {rel: 'canonical', href: 'https://www.ennes.dev'},
     // Define meta tags here.
     meta: [
       {'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8'},
       {description: `
-        Web developer Gustavo Ennes website
+        Full-stack web developer freelancer, node, python, django, RESTful api's, Cloud Functions, 
+        relational and non-relational databases. Check out my portfolio to see my projects!
       `},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'description', content: 'Gustavo Ennes website'},
+      {name: 'description', content:  `
+        Full-stack web developer freelancer, node, python, django, RESTful api's, Cloud Functions, 
+        relational and non-relational databases. Check out my portfolio to see my projects!
+      `},
       {property: 'og:title', content: 'Web developer Gustavo Ennes website'},
       {property: 'og:site_name', content: 'Ennes Dev'},
       // The list of types is available here: http://ogp.me/#types
@@ -72,15 +80,21 @@ export default {
       {property: 'og:url', content: 'Web developer Gustavo Ennes website'},
       //{property: 'og:image', content: 'https://www.my-site.com/my-special-image.jpg'},
       // Often the same as your meta description, but not always.
-      {property: 'og:description', content: 'Gustavo Ennes portfolio'},
+      {property: 'og:description', content:  `
+        Full-stack web developer freelancer, node, python, django, RESTful api's, Cloud Functions, 
+        relational and non-relational databases. Check out my portfolio to see my projects!
+      `},
 
       
       // Google / Schema.org markup:
       {itemprop: 'name', content: 'ennes.dev'},
-      {itemprop: 'description', content: 'Web developer Gustavo Ennes website'},
+      {itemprop: 'description', content:  `
+        Full-stack web developer freelancer, node, python, django, RESTful api's, Cloud Functions, 
+        relational and non-relational databases. Check out my portfolio to see my projects!
+      `},
       // {itemprop: 'image', content: 'https://www.my-site.com/my-special-image.jpg'}
       {itemprop: 'jobTitle', content: 'full-stack web developer'},
-      {itemprop: 'knowsAbout', content: 'web development'},
+      {itemprop: 'knowsAbout', content: 'web development, javascript, vue, node, python, django'},
       {itemprop: 'aditionalName', content: 'Web developer Gustavo Ennes website'},
       {itemprop: 'address', content: 'ilha solteira, são paulo - brasil'},
       {itemprop: 'email', content: 'gustavo@ennes.dev'},
